ff053483bd0c93c01271fc1db52d4cee1de405da
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-09-22 21:36:55 +0600

    next autx beta virsion and login added

---
+++ auth.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { User } from "./model/user-model";
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        if (credentials == null) return null;
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          console.log(user);
+          if (user) {
+            const isMatch = await bcrypt.compare(
+              user?.password,
+              credentials?.password
+            );
+            if (isMatch) {
+              return user;
+            } else {
+              console.log("Password mismatch");
+              throw new Error("Check your password");
+            }
+          } else {
+            console.log("User not found!");
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  ],
+});
