6a18db4f858904ed0df53e679f4420d0d85fe26f
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-10-25 15:48:21 +0600

    stripe payment medthod and enroll success page added

--- components/enrollment-course.jsx
+++ components/enrollment-course.jsx
@@ -1,24 +1,34 @@
+"use client";
+import { createCheckoutSession } from "@/app/actions/stripe";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import { Button, buttonVariants } from "./ui/button";
 
 export const EnrollmentCourse = ({ asLink }) => {
+  /* handlaFormAction  */
+  const handlaFormAction = async (data) => {
+    const { url } = await createCheckoutSession(data);
+
+    window.location.assign(url);
+  };
   return (
     <>
-      {asLink ? (
-        <Button
-          variant='ghost'
-          type='submit'
-          className='text-xs text-sky-700 h-7 gap-1'
-        >
-          Enroll
-          <ArrowRight className='w-3' />
-        </Button>
-      ) : (
-        <Button href='' className={cn(buttonVariants({ size: "lg" }))}>
-          Enroll Now
-        </Button>
-      )}
+      <form action={handlaFormAction}>
+        {asLink ? (
+          <Button
+            variant='ghost'
+            type='submit'
+            className='text-xs text-sky-700 h-7 gap-1'
+          >
+            Enroll
+            <ArrowRight className='w-3' />
+          </Button>
+        ) : (
+          <Button href='' className={cn(buttonVariants({ size: "lg" }))}>
+            Enroll Now
+          </Button>
+        )}
+      </form>
     </>
   );
 };
