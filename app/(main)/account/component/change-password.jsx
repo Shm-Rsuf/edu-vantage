2de57e35be0a4164c4d1eb8a31cb22b0e6712bd5
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-11-24 23:47:24 +0600

    work on change password file

--- app/(main)/account/component/change-password.jsx
+++ app/(main)/account/component/change-password.jsx
@@ -1,32 +1,68 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
-const ChangePassword = () => {
+const ChangePassword = ({ email }) => {
+  const [passwordState, setPasswordState] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+
+  /* handle change function */
+  const handleChange = (event) => {
+    event.preventDefault();
+    const field = event.target.name;
+    const value = event.target.value;
+
+    setPasswordState({ ...passwordState, [field]: value });
+  };
+
+  /* handle Form Submit */
+
+  const doPasswordChage = async (event) => {
+    event.preventDefault();
+    console.log(passwordState);
+  };
   return (
     <div>
       <h5 className='text-lg font-semibold mb-4'>Change password :</h5>
-      <form>
+      <form onSubmit={doPasswordChage}>
         <div className='grid grid-cols-1 gap-5'>
           <div>
             <Label className='mb-2 block'>Old password :</Label>
-            <Input type='password' placeholder='Old password' required='' />
+            <Input
+              type='password'
+              placeholder='Old password'
+              name='oldPassword'
+              id='oldPassword'
+              required
+              onChange={handleChange}
+            />
           </div>
           <div>
             <Label className='mb-2 block'>New password :</Label>
-            <Input type='password' placeholder='New password' required='' />
+            <Input
+              type='password'
+              placeholder='New password'
+              name='newPassword'
+              id='newPassword'
+              required
+              onChange={handleChange}
+            />
           </div>
           <div>
             <Label className='mb-2 block'>Re-type New password :</Label>
             <Input
               type='password'
               placeholder='Re-type New password'
-              required=''
+              required
             />
           </div>
         </div>
         {/*end grid*/}
-        <Button className='mt-5' type='submit'>
+        <Button className='mt-5 cursor-pointer' type='submit'>
           Save password
         </Button>
       </form>
